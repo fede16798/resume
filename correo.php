7e07e1e35f141cc7e8900db6cdefe6692b3520d3
Federico Amico <fede.amico98@gmail.com>
2022-02-14 19:58:00 -0300

    email sent now is verifying in javascript and php, also there is a span when it fails or not

--- correo.php
+++ correo.php
@@ -10,7 +10,14 @@
     $mensajeCompleto .= "Reply-To: $email \n";
     $mensajeCompleto .= "Atte: $name";
 
-    $mail = @mail($destinatario, $asunto, $mensajeCompleto);
-    echo "<script>alert('Correo enviado exitosamente')</script>";
-    echo "<script> setTimeout(\"location.href='index.html'\", 1000) </script>";
-?> 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if((!empty($name) && strlen($name) > 4) && 
+            (!empty($asunto) && strlen($asunto) > 4) && 
+            (!empty($mensaje) && strlen($mensaje) > 4)) {
+        $mail = @mail($destinatario, $asunto, $mensajeCompleto);
+        echo "<script>alert('Correo enviado exitosamente')</script>";
+        echo "<script> setTimeout(\"location.href='index.html'\", 1000) </script>";
+        }
+    } 
+    
+?>
--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
+const boton = document.getElementById("icono");
+const enlaces = document.getElementById("enlaces");
 let contador = 0;
+
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const asunto = document.getElementById("asunto");
+const message = document.getElementById("mensaje");
+const button = document.getElementById("btn-send");
+const resultado = document.querySelector(".resultado");
+const footer = document.getElementById("contact");
+let divAdded = false;
+const fragment = document.createDocumentFragment();
+const div = document.createElement('DIV');
 
 boton.addEventListener('click', function(){
     if (contador == 0) {
@@ -16,3 +27,49 @@ boton.addEventListener('click', function(){
     }
 })
 
+button.addEventListener('click', (e) => {
+    let error = checkForm();
+
+    
+    div.classList.add('resultado');
+    if (error[0]){
+        div.innerHTML = error[1];
+        div.classList.add("wrongForm");
+        div.classList.remove("correctForm");
+    } else {
+        div.innerHTML = "Mail enviado correctamente";
+        div.classList.add("correctForm");
+        div.classList.remove("wrongForm");
+    }
+    if(!divAdded){ 
+        fragment.appendChild(div);
+        footer.appendChild(fragment);
+        divAdded = true;
+    }
+})
+
+const checkForm = () => {
+    let error = [];
+    if (nombre.value.length < 5) {
+        error[0] = true;
+        error[1] = "El nombre tiene que tener mas de 4 caracteres";
+        return error;
+    } else if (email.value.length < 5 || 
+            email.value.length > 40 || 
+            email.value.indexOf("@")== -1 || 
+            email.value.indexOf(".") == -1) {
+        error[0] = true;
+        error[1] = "El mail es invalido.";
+        return error;
+    } else if (asunto.value.length < 4) {
+        error[0] = true;
+        error[1] = "El asunto es demasiado corto";
+        return error;
+    }else if (message.value.length < 4) {
+        error[0] = true;
+        error[1] = "El mensaje es demasiado corto";
+        return error;
+    }
+    error[0] = false;
+    return error;
+}
